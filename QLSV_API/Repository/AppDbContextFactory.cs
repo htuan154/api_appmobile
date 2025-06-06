d7db706ebc3ff643c73d69613d311c5f28688291
htuan <htuan15424@gmail.com>
2025-06-07 03:28:53 +0700

    Fix Render Data 4

--- QLSV_API/Repository/AppDbContextFactory.cs
+++ QLSV_API/Repository/AppDbContextFactory.cs
@@ -16,7 +16,11 @@ namespace QLSV_API.Repository
 
 			var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
 			var connectionString = configuration.GetConnectionString("DefaultConnection");
-
+			// Xử lý URL encoding cho password nếu cần
+			if (connectionString.Contains("htuan15424@") && !connectionString.Contains("htuan15424%40"))
+			{
+				connectionString = connectionString.Replace("htuan15424@", "htuan15424%40");
+			}
 			// Đổi từ UseSqlServer sang UseNpgsql cho PostgreSQL
 			optionsBuilder.UseNpgsql(connectionString);
 
